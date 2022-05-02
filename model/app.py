f89b6e998f42c6b1a95990682f86f3c45d8236a2
ox7f <ox7f@protonmail.com>
2022-05-02 19:25:41 +0200

    Co-authored-by: erwinbause23@gmail.com <erwinbause23@gmail.com>

--- model/app.py
+++ model/app.py
@@ -1,5 +1,6 @@
 from config import CONFIG
 
+# von ordner 'model' importiere datei 'frame'
 from model.frame import FRAME
 from model.button import BUTTON
 from model.graph import Graph
@@ -8,32 +9,34 @@ from model.menu import MENU
 import tkinter as tk
 import tkinter.ttk as ttk
 
-class App(tk.Tk):
-    menu = None
-    frames = []
-    frame_data = []
-    active_tab = 0
-    active_frame = None
-    data = []
+# tk => module
+# tk.TK => klasse
+class App(tk.Tk): #Erstellung der Klasse App und Vererbung von tkinter Klasse
+    #Variablen werden direkt beim Start erstellt und nicht erst beim Aufruf der jeweiligen Funktion, sofort im Fenster sichtbar!
+    menu = None #Erstellung der Variable Menu
+    frames = [] #um mehrere Frames in einer Variable zu speichern
+    frame_data = [] #Sind die Daten von CYCLODATA ODER SQUAREWAVEDATA (config), je nach dem welcher Tab gerade Aktiv ist
+    active_frame = None #ausgewaehlter frame (frame.py), entweder Cyclo oder Squarewave dort sind alle Eingabeboxen mit ihren Wert
+    data = [] # Um alle Werte von der Messung in eine Datei zu speichern; 
 
     # INITIALIZE OBJECT
-    def __init__(_self):
-        super().__init__()
+    def __init__(_self): #Konstruktor -> erstellt dann das Objekt, also das Fenster mit den jeweiligen Widgets 
+        super().__init__() #-> Erben von tk.Tk(Elternklasse) um dann z.B. geometry, title usw. auszuwählen
 
         # window settings
-        _self.geometry('800x400')
-        _self.title('Hegewald GmbH')
-        _self.resizable(False, False)
+        _self.geometry('800x400') #Größe des Fensters
+        _self.title('Hegewald GmbH') #Titel des Fensters
+        _self.resizable(False, False) #Größe des Fensters nicht veränderbar
 
         # create menu
         _self.create_menu()
-
+        #über alle _self, weil es (zur klasse gehoert)
         # create tab
-        _self.create_notebook()
+        _self.create_notebook() #Die Methode zur Erstellung der Tableiste, wo beide Tabs später rein kommen
 
         # create frames
-        _self.create_frame('Cyclovoltametrie', CONFIG['CYCLODATA'])
-        _self.create_frame('Squarewave-Voltametrie', CONFIG['SQUAREWAVEDATA'])
+        _self.create_frame('Cyclovoltametrie', CONFIG['CYCLODATA']) # _self.frames[0]
+        _self.create_frame('Squarewave-Voltametrie', CONFIG['SQUAREWAVEDATA'])  #_self.frames[1]
 
         # create buttons
         _self.create_button(_self.start, 'start', 9, 0)
@@ -42,8 +45,6 @@ class App(tk.Tk):
 
         # create graph
         _self.create_graph()
-
-
 
     # CREATE GUI ELEMENTS
     def create_menu(_self):
@@ -55,7 +56,8 @@ class App(tk.Tk):
         _self.notebook.grid(row=row, column=column)
 
     def create_frame(_self, name, data=[]):
-        _self.frames.append(FRAME(_self, _self.notebook, name, data))
+        frame = FRAME(_self, _self.notebook, name, data) #Erstellung eines Frame Objektes(frame.py), speicherung in Frame
+        _self.frames.append(frame) #Erstelltes Frame Objekt der Liste hinzufuegen
 
     def create_button(_self, click, name, row, column):
         BUTTON(_self, click, name, row, column)
@@ -64,16 +66,15 @@ class App(tk.Tk):
         _self.graph = Graph(_self)
 
 
-
     # TAB EVENTS
     def tab_changed(_self, event):
-        _self.active_tab = _self.notebook.index('current')
-        _self.active_frame = _self.frames[_self.active_tab]
+        active_tab = _self.notebook.index('current') #In a call to the Notebook widget's .index() method, use the string "end" to determine the current number of tabs displayed.
+        _self.active_frame = _self.frames[active_tab] #hole Frame aus liste (index 0 oder 1) (man schaut und holt)
 
-        if (_self.active_tab == 0):
+        if (active_tab == 0):
             _self.frame_data = CONFIG['CYCLODATA']
 
-        if (_self.active_tab == 1):
+        if (active_tab == 1):
             _self.frame_data =  CONFIG['SQUAREWAVEDATA']
 
     # BUTTON EVENTS
@@ -85,8 +86,6 @@ class App(tk.Tk):
 
     def clear(_self):
         _self.clear_values()
-
-
 
     # HELPER
     def get_values(_self):
--- model/frame.py
+++ model/frame.py
@@ -1,13 +1,17 @@
 from tkinter import Frame
 from model.entry import ENTRY
 
+frame = Frame(root)
 class FRAME:
+    frame = None
+    
     def __init__(_self, root, tab=None, text='', data=[]):
         super().__init__()
  
-        _self.frame = Frame(tab)
-        tab.add(_self.frame, text=text)
+        _self.frame = Frame(tab) #erstelle tk frame
+        tab.add(_self.frame, text=text) # konfiguriere tk frame (setze name)
 
         # create entries
-        for index, name in enumerate(data):
-            _self.frame.__dict__[name] = ENTRY(root, _self.frame, name, index)
+        for index, name in enumerate(data): #schleife durch alle noetigen inputfelder 
+            _self.frame.__dict__[name] = ENTRY(root, _self.frame, name, index) # erstelle inputfelder => speichern inputfelder im frame
+            # _self.frame.__dict__ => verwandel klasse in liste
